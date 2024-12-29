c7aecaaca658f6eac625e3f391d4322e40157de2
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-29 16:20:07 -0300

    ✨ Feat: criando rota GET para dados de usuário

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,18 +3,23 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 
 const SECRET_KEY = process.env.JWT_SECRET || "secret-key";
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Token não fornecido." });
+    res.status(401).json({ message: "Token não fornecido." });
+    return;
   }
 
   try {
-    const decoded = jwt.verify(token, SECRET_KEY) as string | JwtPayload;
+    const decoded = jwt.verify(token, SECRET_KEY) as JwtPayload | string;
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ message: "Token inválido ou expirado." });
+    res.status(403).json({ message: "Token inválido ou expirado." });
   }
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authController";
+import { getProfile, login, register } from "../controllers/authController";
+import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", authenticateToken, getProfile);
 
 export default router;

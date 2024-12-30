fb2433b4ea515c10f080ddcbd48ec8a71babbd01
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-30 10:00:11 -0300

    ♻️ Refactor: enviando dados do usuário na respsota do login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -36,7 +36,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
   }
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("-password");
 
     if (!user) {
       res.status(401).json({ message: "Credenciais inválidas." });
@@ -56,7 +56,14 @@ export const login = async (req: Request, res: Response): Promise<void> => {
       { expiresIn: "1h" }
     );
 
-    res.status(200).json({ token });
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Erro ao autenticar usuário." });
